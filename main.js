4c572d63bb1d91143544b0baf198e3d3108cadfb
mahzzaa <62835804+mahzzaa@users.noreply.github.com>
2025-09-27 15:28:17 +0300

    Add CSP meta tags and webPreferences to windows
    Added Content-Security-Policy meta tags to index.html and second.html for improved security. Updated main.js to set nodeIntegration and contextIsolation options in BrowserWindow webPreferences for both main and second windows.

--- main.js
+++ main.js
@@ -11,8 +11,22 @@ let mainWindow;
 let secondWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({ width: 800, height: 600 });
-  secondWindow = new BrowserWindow({ width: 400, height: 300 });
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+  secondWindow = new BrowserWindow({
+    width: 400,
+    height: 300,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
 
   mainWindow.loadURL(
     url.format({
